2db2a266c590734ebbd8e70358df15bbe3af4058
Ana Carlos <adscarlos@gmail.com>
2017-12-13 12:17:16 +0000

    Add Node checking in build script

--- jakefile.js
+++ jakefile.js
@@ -2,9 +2,23 @@
   "use strict";
 // to see list of tasks and their description: `jake -T`
 
+var EXPECTED_NODE_VERSION = "v9.2.1";
+
   desc("Default build");
-  task("default", function() {
+  task("default", [ "version" ], function() {
     console.log("\n\nBUILD OK");
+  });
+
+  desc("Check Node version");
+  task("version", function() {
+    console.log("Checking node version: .");
+
+    let actualVersion = process.version;
+    if ( actualVersion !== EXPECTED_NODE_VERSION ) {
+      fail( "Incorrect Node version: expected " +
+      EXPECTED_NODE_VERSION +
+      ", but was " + actualVersion );
+  }
   });
 
 }());
